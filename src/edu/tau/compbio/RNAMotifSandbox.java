a7f6f357247375938ce35dd2c8a15374cf324739
maordan <maordan@mail.tau.ac.il>
2015-06-03 14:00:49 +0300

    added use of RNAcompete to sandbox

--- src/edu/tau/compbio/RNAMotifSandbox.java
+++ src/edu/tau/compbio/RNAMotifSandbox.java
@@ -1,32 +1,39 @@
 package edu.tau.compbio;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Vector;
 
 import com.ibm.compbio.seqalign.CustomSmithWaterman;
 
+import edu.tau.compbio.util.RNACompeteScorer;
 import edu.tau.compbio.util.RNAWithPairingProbabilities;
 
 public class RNAMotifSandbox {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		int match = 5;
 		int mismatch = -4;
 		int gap = -10;
 		
-		Vector<RNAWithPairingProbabilities> data = RNAWithPairingProbabilities.readFromFiles(args[0], args[1]); 
+		Vector<RNAWithPairingProbabilities> data = RNAWithPairingProbabilities.readFromFiles(args[0], args[1]);
+		RNACompeteScorer rnaCompete = new RNACompeteScorer(new File("W:\\master\\alignerTest\\z_scores.txt"), "RNCMPT00121_z_setA");
 		
 		int i = 0;
 		for (RNAWithPairingProbabilities rna1 : data)
 		{
-			for (RNAWithPairingProbabilities rna2 : data)
+			for (RNAWithPairingProbabilities rna2 : data.subList(data.indexOf(rna1)+1, data.size()))
 			{
 				CustomSmithWaterman a  = new CustomSmithWaterman(rna1.getSequenceArray() ,rna2.getSequenceArray(), match, mismatch, gap, rna1.getProbabilitiesArray(), rna2.getProbabilitiesArray());
 				String[] res=  a.getAlignedSequences();
-				System.out.println(res[0] + "\t" + res[1]);
+				//System.out.println(res[0] + "\t" + res[1]);
+				
+				System.out.printf("%f\t%f\n",rnaCompete.getScoreForSubsequence(res[0]),rnaCompete.getScoreForSubsequence(res[1]));
+				
 				i++;
-				if (i>0)
+				if (i>1000)
 				{
-					return;
+				//	return;
 				}
 			}			
 		}
--- src/edu/tau/compbio/util/RNACompeteScorer.java
+++ src/edu/tau/compbio/util/RNACompeteScorer.java
@@ -5,40 +5,113 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.Vector;
 
 public class RNACompeteScorer {
 
+	private Vector<Double> m_scores;
+	private double m_normalizationFactor;
+	private static final int s_k = 7;
 	
 	public RNACompeteScorer(File inputData, String proteinID) throws FileNotFoundException, IOException
 	{
+		m_scores = new Vector<Double>(32);
 		BufferedReader lineScanner = new BufferedReader(new FileReader(inputData));
 		String line = lineScanner.readLine();
 		Scanner valuesScanner = new Scanner(line).useDelimiter("\\s+");
 		Vector<String> titles = new Vector<String>(32);
-		for(String title :() { valuesScanner })
+		while( valuesScanner.hasNext() )
 		{
-			titles.add(title);
+			titles.add(valuesScanner.next());
 		}
+		int proteinCol = titles.indexOf(proteinID);
 		while (null != (line = lineScanner.readLine()))
 		{
+			valuesScanner = new Scanner(line).useDelimiter("\\s+");
+			for (int i=0; i<proteinCol-1; i++)
+			{
+				valuesScanner.next();
+			}
+			m_scores.add(valuesScanner.nextDouble());
 			
 		}
 	}
 	
 	public void setRefSequence(String seq)
 	{
-		//TODO		
+		m_normalizationFactor = Collections.max(m_scores);
 	}
 	
 	public double getScoreForSubsequence(String subSeq)
 	{
-		//TODO
+		StringBuffer sb = new StringBuffer(subSeq.substring(0, 7));
+		double maxScore = m_scores.elementAt(kMerToIndex(sb.toString()));
 		
+		for(char c : subSeq.substring(7).toCharArray())
+		{
+			sb.append(c);
+			sb.deleteCharAt(0);
+			maxScore = Math.max(maxScore, m_scores.elementAt(kMerToIndex(sb.toString())));
+		}
 		
-		return 0;
+		return maxScore / m_normalizationFactor;
+	}
+	
+	private int kMerToIndex(String kmer)
+	{
+		int index = 0;
+		for (char c : kmer.toCharArray())
+		{
+			index *= 4;
+			index += baseToDigit(c);
+		}
+		return index;
+	}
+	
+	private int baseToDigit(char base)
+	{
+		switch(base)
+		{
+		case 'A':
+			return 0;
+		case 'C':
+			return 1;
+		case 'G':
+			return 2;
+		case 'T':
+		case 'U':
+			return 3;
+		}
+		return -1;
+	}
+	
+	private String indexToKMer(int index, int k)
+	{
+		StringBuffer sb = new StringBuffer();
+		for(int i=0; i<k ;i++)
+		{
+			sb.insert(0,digitToBase(index%4));
+			index /= 4;
+		}
+		return sb.toString();
 	}
 	
 	
+	private char digitToBase(int digit)
+	{
+		switch(digit)
+		{
+		case 0:
+			return 'A';
+		case 1:
+			return 'C';
+		case 2:
+			return 'G';
+		case 3:
+			return 'U';
+		}
+		return 0;
+	}
 }
